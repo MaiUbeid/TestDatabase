ca0e8e8473c5bb9690a1c079fb46bdb0c6571147
MaiUbeid <ubead95@gmail.com>
2019-08-24 23:50:49 +0300

    fix get data and change name to user_name

--- server/database/queries/addCity.js
+++ server/database/queries/addCity.js
@@ -1,10 +1,10 @@
 const dbconnection = require('../config/connection');
 
 exports.addCity = (info) => {
-  const { name, city } = info;
+  const { user_name, city } = info;
   const sql = {
-    text: 'INSERT INTO cities (name, city) VALUES ($1, $2)',
-    values: [name, city] 
+    text: 'INSERT INTO cities (user_name, city) VALUES ($1, $2)',
+    values: [user_name, city] 
   };
   return dbconnection.query(sql);
 };
